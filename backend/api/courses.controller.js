b6454cf5c37882fd21b004f5adf7de028232c990
pppedro <pedropaulomlt@gmail.com>
2022-03-06 21:20:55 -0300

    adição de criação de rercurso do tipo curso

--- backend/api/courses.controller.js
+++ backend/api/courses.controller.js
@@ -1,5 +1,45 @@
-import CoursesDAO from "../dao/coursesDAO.js"
 
+import {Course} from "../models/course.js"
+
+class coursesDao{
+    static async getCourses({
+        filters = null,
+        page = 0,
+        coursesPerPage = 10,
+        } = {}) {
+        let query
+        if (filters) {
+            if ("name" in filters){
+                query = {
+                    $text: { search: filters["name"]}
+                }
+            }
+        }
+        let cursor
+
+        try {
+            cursor = await Course.find( query)
+
+        }catch( e){
+            console.error(`Unable to issue find command, ${e}`)
+            return { coursesList: [], numCourses: 0}
+        }
+        //get courses for specific page
+        const displayCursor = cursor.limit(coursesPerPage).skip( page * coursesPerPage)
+
+        try{
+            const coursesList = await displayCursor.toArray()
+            const numCourses = await courses.countDocuments( query)
+            
+            return { coursesList, numCourses}
+        }catch ( e){
+            console.error(
+                `Unable to convert cursor to array or count the number of documents, ${e}`
+            )
+            return { coursesList: [], numCourses: 0}
+        }
+    }
+}
 export default class CoursesCtrl{
     static async apiGetCourses( req, res, next){
         const coursesPerPage = req.query.coursesPerPage ? parseInt( req.query.coursesPerPage, 10) : 10
@@ -10,7 +50,7 @@ export default class CoursesCtrl{
             filters.name = req.query.name
         } 
 
-        const { coursesList, numCourses} = await CoursesDAO.getCourses({
+        const { coursesList, numCourses} = await coursesDao.getCourses({
             filters,
             page,
             coursesPerPage
@@ -25,5 +65,22 @@ export default class CoursesCtrl{
             total_results: numCourses
         }
         res.json( response)
+    }
+    static async apiCreateCourse( req, res, next){
+        //create a active new course 
+        try{
+            
+            const course = new Course({
+                name: req.query.name,
+                password: req.query.password,
+                status: 'ativo'
+            })
+
+            const response = await course.save()
+                
+            res.json(response)
+        }catch( e){
+            console.error( e)
+        }
     }
 }

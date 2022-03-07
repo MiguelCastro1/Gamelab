aeefb318b5aab7b098e522a032d4c92d22e6f7b7
Rodrigo <rodrigotaveiramelo@gmail.com>
2022-03-07 19:01:11 -0400

    integração com o back: input, cadastro user

--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { useField } from "formik";
 // import styles from "./styles.scss";
 import styles from "./styles.module.scss";
@@ -34,3 +35,30 @@ function Input({ label, placeholder, width, isForm, ...props }) {
 }
 
 export default Input;
+=======
+import { useField } from "formik";
+// import styles from "./styles.scss";
+import styles from "./styles.module.scss";
+
+function Input({label,  ...props }) {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <div className={styles.containerInput}>
+        <input
+          {...field}
+          {...props}
+          style={{ border: meta.touched && meta.error && "1px solid red" }}
+        />
+        <span>{label}</span>
+        {meta.touched && meta.error ? (
+          <div className={styles.error}>{meta.error}</div>
+        ) : null}
+      </div>
+      <></>
+    </>
+  );
+}
+
+export default Input;
+>>>>>>> 6e8b9e8b23edc3f39918767f1fef958330ad467a
--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -12,12 +12,30 @@ import HomeProfessor from "./Pages/HomeProfessor";
 import HomeAluno from "./Pages/HomeAluno";
 import Login from "./Pages/Login";
 import DetalheTurma from "./Pages/DetalheTurma";
+<<<<<<< HEAD
 import { isAuthenticated } from "./services/auth";
 
 const RotasPrivadas = () => {
   const isAuth = isAuthenticated();
   return isAuth ? <Outlet /> : <Navigate to="/login" />;
 };
+=======
+//import { isAuthenticated } from "./services/auth";
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      isAuthenticated() ? (
+        <Component {...props} />
+      ) : (
+        <navigateTo to={{ pathname: "/", state: { from: props.location } }} />
+      )
+    }
+  />
+);
+import HomeAluno from "./Pages/HomeAluno";
+>>>>>>> 6e8b9e8b23edc3f39918767f1fef958330ad467a
 
 export default function Rotas() {
   return (
@@ -27,11 +45,18 @@ export default function Rotas() {
         <Route path="/cadastrar" element={<EscolherPerfil />} />
         <Route path="/cadastrar-perfil" element={<FormCadastro />} />
         <Route path="/email" element={<EmailEnviado />} />
+<<<<<<< HEAD
         <Route element={<RotasPrivadas />}>
           <Route path="/" element={<HomeProfessor />} />
           <Route path="/detalhes" element={<DetalheTurma />} />
           <Route path="/home2" element={<HomeAluno />} />
         </Route>
+=======
+        <Route path="/" element={<DetalheTurma />} />
+        <Route path="/home" element={<HomeProfessor />} />
+        <Route path='*' element={<h1>Not Found</h1>} />
+        <Route path="/home2" element={<HomeAluno />} />
+>>>>>>> 6e8b9e8b23edc3f39918767f1fef958330ad467a
       </Routes>
     </BrowserRouter>
   );

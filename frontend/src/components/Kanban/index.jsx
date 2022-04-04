1372ba30a67d5fce56ce078ff7dfe68349539921
nataliasousafreire <nsf@icomp.ufam.edu.br>
2022-04-04 15:27:08 -0400

    kanbam

--- frontend/src/components/Kanban/index.jsx
+++ frontend/src/components/Kanban/index.jsx
@@ -1,11 +1,53 @@
 
 import styles from "./styles.module.scss";
+import Board from '@asseinfo/react-kanban'
+import  '@asseinfo/react-kanban/dist/styles.css'
 
 function kanban() {
+  const  board  =  { 
+  columns : [ 
+    { 
+      id : 1 , 
+      title : 'To Do' , 
+      cards : [ 
+        { 
+          id : 1 , 
+          title : 'Fazer testes' , 
+          description : 'Fazer testes de usabilidade no site Gamelab' 
+        } , 
+      ] 
+    } , 
+    { 
+      id : 2 , 
+      title : 'Doing' , 
+      cards : [ 
+        { 
+          id : 2, 
+          title : 'Apresentação da sprint2' , 
+          description : 'Explicar para a professora oque foi feito na sprint2' 
+        } , 
+      ] 
+    },
+     { 
+      id : 3 , 
+      title : 'Done' , 
+      cards : [ 
+        { 
+          id : 3, 
+          title : 'Integrar criar aviso' , 
+          description : 'Integrar criar aviso com email' 
+        } , 
+      ] 
+    },
+     
+    
+  ]
+  } 
   return (
 
       <div className={styles.feed}>
         <h1>Kanban</h1>
+        < Board  initialBoard = { board }  / >
       </div>      
   );
 }

3a745db1ba666c3457b012896004ad7edbcf4369
Miguel Castro <miguelcastro0207@gmail.com>
2022-04-04 18:58:12 -0300

    solving merge

--- frontend/src/components/Participantes/index.jsx
+++ frontend/src/components/Participantes/index.jsx
@@ -1,24 +1,10 @@
 import { Link } from "react-router-dom";
 import userPhoto from "../../assets/user_padrao.png";
 import styles from "./styles.module.scss";
-<<<<<<< HEAD
-import { FcConferenceCall } from "react-icons/fc";
-=======
 import {FcConferenceCall} from 'react-icons/fc'
 import { useEffect, useState } from "react";
 import api from "../../services/axios";
 import { setNestedObjectValues } from "formik";
-
-
-
-function Participantes({Alunos, ...props}) {
-  const [imgUser, setImgUser] = useState({});
-
-  
-
-
-  return (
->>>>>>> d25823e9a9947536e928ad36512560e5d13c85d2
 
 function Participantes({ Alunos, ...props }) {
   return (
@@ -28,12 +14,6 @@ function Participantes({ Alunos, ...props }) {
         <FcConferenceCall size={35} /> Participantes
       </h1>
       {Alunos.map((aluno) => (
-<<<<<<< HEAD
-        <Link key={aluno.userId._id} to={`/perfil/${aluno.userId._id}`}>
-          <div className={styles.aluno}>
-            <img src={userPhoto} alt="Perfil" width={50} height={50} />
-            <h2> {aluno.userId.nome} </h2>
-=======
           <Link key={aluno.userId._id} to={`/perfil/${aluno.userId._id}`}>
           <div  className={styles.aluno}>
             <img 
@@ -43,7 +23,6 @@ function Participantes({ Alunos, ...props }) {
               height={50}
               />
             <h2 > {aluno.userId.nome} </h2>
->>>>>>> d25823e9a9947536e928ad36512560e5d13c85d2
           </div>
         </Link>
       ))}
